7dde6d784b3e9c82719b3b308c34b5c2ef5db727
dmathieu <42@dmathieu.com>
2010-12-13 14:48:08 +0100

    continue with the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
+  before_filter :get_post, :only => [:show, :edit, :update]
+  
+  def show
+  end
   
   def new
     @post = Post.new
+  end
+  
+  def edit
+    render :new
   end
   
   def create
@@ -12,5 +20,19 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+  
+  def update
+    if @post.update_attributes params[:post]
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+  
+  
+  private
+  def get_post
+    @post = Post.find params[:id]
   end
 end
